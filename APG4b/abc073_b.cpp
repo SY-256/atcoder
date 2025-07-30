a62206311cc5045db7be3063936d2a7a728582cb
SY-256 <yamamotosw1005@gmail.com>
2025-07-30 13:46:46 +0000

    7/30 commit

---
+++ APG4b/abc073_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n + 1); i++)
+using ll = long long;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    int l, r;
+    cin >> l >> r;
+    ans += (r - l) + 1;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
